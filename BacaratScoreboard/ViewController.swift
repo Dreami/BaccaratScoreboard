d1485e4b2bd5ffd238a9cd76d10f67932f7e4fa5
Alumno <Alumno@imac-de-informatica-5.local>
2017-10-23 11:56:06 -0500

    Initial Commit

---
+++ BacaratScoreboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BacaratScoreboard
+//
+//  Created by Alumno on 17/10/17.
+//  Copyright © 2017 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var stack1: UIStackView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
